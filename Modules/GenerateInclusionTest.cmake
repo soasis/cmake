940ff1a710a0bb5ec92db9f2fad659c356647425
ThePhD <phdofthehouse@gmail.com>
2025-04-17 18:01:00 +0200

    🛠 More powerful inclusion tests

--- Modules/GenerateInclusionTest.cmake
+++ Modules/GenerateInclusionTest.cmake
@@ -96,7 +96,7 @@ set(ztd_cmake_inclusion_test_main_file [==[
 //
 // ============================================================================ //
 
-int main() { return 0; }
+int main(int argc, char* argv[]) { (void)argc; (void)argv; return 0; }
 
 ]==])
 
@@ -104,52 +104,67 @@ int main() { return 0; }
 Generates a set of tests whose job is to include a single file.
 ]]
 function (generate_inclusion_test)
-	set(options)
-	set(oneValueArgs ROOT NAME)
-	set(multiValueArgs LINK_LIBRARIES)
+	set(options INCLUDE_DETAIL)
+	set(oneValueArgs BASE_DIRECTORY NAME)
+	set(multiValueArgs LINK_LIBRARIES ROOTS)
 	cmake_parse_arguments(PARSE_ARGV 0 arg
 		"${options}" "${oneValueArgs}" "${multiValueArgs}"
 	)
 
-	cmake_path(ABSOLUTE_PATH arg_ROOT BASE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} NORMALIZE OUTPUT_VARIABLE ${arg_ROOT})
-	file(GLOB_RECURSE inclusion_test_c_files
-		LIST_DIRECTORIES false
-		CONFIGURE_DEPENDS
-		${arg_ROOT}/**.h)
-	file(GLOB_RECURSE inclusion_test_c++_files
-		LIST_DIRECTORIES false
-		CONFIGURE_DEPENDS
-		${arg_ROOT}/**.hpp
-		${arg_ROOT}/**.hxx
-		${arg_ROOT}/**.h++
-		${arg_ROOT}/**.hh)
-	list(FILTER inclusion_test_c_files EXCLUDE REGEX "/detail/")
-	list(FILTER inclusion_test_c++_files EXCLUDE REGEX "/detail/")
-	foreach(c_file ${inclusion_test_c_files})
-		cmake_path(CONVERT ${c_file} TO_CMAKE_PATH_LIST c_file NORMALIZE)
-		cmake_path(RELATIVE_PATH c_file BASE_DIRECTORY ${arg_ROOT} OUTPUT_VARIABLE base_c_file)
-		set(INCLUDE_FILE ${c_file})
-		set(generated_c_file "${CMAKE_CURRENT_BINARY_DIR}/source/${base_c_file}.c")
-		set(generated_c_c++_file "${CMAKE_CURRENT_BINARY_DIR}/source/${base_c_file}.c.cpp")
-		file(CONFIGURE OUTPUT ${generated_c_file}
-			CONTENT "${ztd_cmake_inclusion_test_config_file}"
-			@ONLY)
-		list(APPEND inclusion_test_generated_files ${generated_c_file})
-		file(CONFIGURE OUTPUT ${generated_c_c++_file}
-			CONTENT "${ztd_cmake_inclusion_test_config_file}"
-			@ONLY)
-		list(APPEND inclusion_test_generated_files ${generated_c_c++_file})
+	foreach (root ${arg_ROOTS})
+		cmake_path(ABSOLUTE_PATH root BASE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} NORMALIZE OUTPUT_VARIABLE ${root})
+		file(GLOB_RECURSE root_inclusion_test_c_files
+			LIST_DIRECTORIES false
+			CONFIGURE_DEPENDS
+			${root}/**.h)
+		file(GLOB_RECURSE root_inclusion_test_c++_files
+			LIST_DIRECTORIES false
+			CONFIGURE_DEPENDS
+			${root}/**.hpp
+			${root}/**.hxx
+			${root}/**.h++
+			${root}/**.hh)
+		if (NOT ${arg_INCLUDE_DETAIL})
+			list(FILTER root_inclusion_test_c_files EXCLUDE REGEX "/detail/")
+			list(FILTER root_inclusion_test_c++_files EXCLUDE REGEX "/detail/")
+		endif()
+	
+		if (arg_BASE_DIRECTORY)
+			set(base_dir ${arg_BASE_DIRECTORY})
+		else()
+			set(base_dir ${root})
+		endif()
+		if (NOT IS_ABSOLUTE ${base_dir})
+			cmake_path(ABSOLUTE_PATH base_dir BASE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} NORMALIZE OUTPUT_VARIABLE base_dir)
+		endif()
+
+		foreach(c_file ${root_inclusion_test_c_files})
+			cmake_path(CONVERT ${c_file} TO_CMAKE_PATH_LIST c_file NORMALIZE)
+			cmake_path(RELATIVE_PATH c_file BASE_DIRECTORY ${base_dir} OUTPUT_VARIABLE base_c_file)
+			set(INCLUDE_FILE ${c_file})
+			set(generated_c_file "${CMAKE_CURRENT_BINARY_DIR}/source/${base_c_file}.c")
+			set(generated_c_c++_file "${CMAKE_CURRENT_BINARY_DIR}/source/${base_c_file}.c.cpp")
+			file(CONFIGURE OUTPUT ${generated_c_file}
+				CONTENT "${ztd_cmake_inclusion_test_config_file}"
+				@ONLY)
+			list(APPEND inclusion_test_generated_files ${generated_c_file})
+			file(CONFIGURE OUTPUT ${generated_c_c++_file}
+				CONTENT "${ztd_cmake_inclusion_test_config_file}"
+				@ONLY)
+			list(APPEND inclusion_test_generated_files ${generated_c_c++_file})
+		endforeach()
+		foreach(c++_file ${root_inclusion_test_c++_files})
+			cmake_path(CONVERT ${c++_file} TO_CMAKE_PATH_LIST c++_file NORMALIZE)
+			cmake_path(RELATIVE_PATH c++_file BASE_DIRECTORY ${base_dir} OUTPUT_VARIABLE base_c++_file)
+			set(INCLUDE_FILE ${c++_file})
+			set(generated_c++_file "${CMAKE_CURRENT_BINARY_DIR}/source/${base_c++_file}.cpp")
+			file(CONFIGURE OUTPUT ${generated_c++_file}
+				CONTENT "${ztd_cmake_inclusion_test_config_file}"
+				@ONLY)
+			list(APPEND inclusion_test_generated_files ${generated_c++_file})
+		endforeach()
 	endforeach()
-	foreach(c++_file ${inclusion_test_c++_files})
-		cmake_path(CONVERT ${c++_file} TO_CMAKE_PATH_LIST c++_file NORMALIZE)
-		cmake_path(RELATIVE_PATH c++_file BASE_DIRECTORY ${arg_ROOT} OUTPUT_VARIABLE base_c++_file)
-		set(INCLUDE_FILE ${c++_file})
-		set(generated_c++_file "${CMAKE_CURRENT_BINARY_DIR}/source/${base_c++_file}.cpp")
-		file(CONFIGURE OUTPUT ${generated_c++_file}
-			CONTENT "${ztd_cmake_inclusion_test_config_file}"
-			@ONLY)
-		list(APPEND inclusion_test_generated_files ${generated_c++_file})
-	endforeach()
+	
 	set(generated_main_file "${CMAKE_CURRENT_BINARY_DIR}/source/main.c")
 	file(CONFIGURE OUTPUT ${generated_main_file}
 		CONTENT "${ztd_cmake_inclusion_test_main_file}"
